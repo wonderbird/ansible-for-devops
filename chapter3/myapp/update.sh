a017b8ac0197bbda0b1bc4d2662caafa121e926f
Stefan Boos <kontakt@boos.systems>
2025-05-01 15:50:32 +0200

    fix update script in version controlled app

--- chapter3/myapp/update.sh
+++ chapter3/myapp/update.sh
@@ -3,4 +3,6 @@
 # After having pulled the latest changes from the git repository, this script
 # performs post-update tasks.
 #
+Hier weiter: SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
 /bin/bash -c "./log_successful_update.sh"
